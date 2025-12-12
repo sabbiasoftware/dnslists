8890d2da8febd5897badc66dfbe31ce04e204436
Sabbia Soft <sabbiasoftware@gmail.com>
2025-12-12 20:11:30 +0100

    sort.py +1

--- sort.py
+++ sort.py
@@ -1,7 +1,9 @@
 import subprocess
 
 queryres = subprocess.run(
-    'sudo sqlite3 /etc/pihole/pihole-FTL.db "select distinct domain from queries order by timestamp desc limit 32"'
+    'sudo sqlite3 /etc/pihole/pihole-FTL.db "select distinct domain from queries order by timestamp desc limit 32"',
+    shell=True,
+    capture_output=True,
 )
 domains = queryres.stdout.decode("utf-8").split("\n")
 for domain in domains:
