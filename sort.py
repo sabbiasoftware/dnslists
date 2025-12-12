9e5e55a6f94aed9a33e3cc8f1f9708dc7589feab
Sabbia Soft <sabbiasoftware@gmail.com>
2025-12-12 19:58:22 +0100

    added sort.py

---
+++ sort.py
@@ -0,0 +1,8 @@
+import subprocess
+
+queryres = subprocess.run(
+    "sudo sqlite3 'select distinct domain from queries order by timestamp desc limit 32'"
+)
+domains = queryres.stdout.decode("utf-8").split("\n")
+for domain in domains:
+    print(domain)
