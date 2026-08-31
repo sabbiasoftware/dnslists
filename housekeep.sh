2023c54b0557026a637d958ba9f8dd6f0ec9a7a1
Sabbia Soft <sabbiasoftware@gmail.com>
2026-08-31 17:40:35 +0200

    added housekeep.sh

---
+++ housekeep.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+sudo systemctl stop pihole-FTL.service
+sudo sqlite3 /etc/pihole/pihole-FTL.db ".backup /home/pi/pihole/pihole-FTL.bak.db"
+sudo sqlite3 /etc/pihole/pihole-FTL.db "DELETE FROM query_storage WHERE timestamp <= strftime('%s', datetime('now', '-30 day'));"
+sudo sqlite3 /etc/pihole/pihole-FTL.db "VACUUM;"
+sudo systemctl start pihole-FTL.service
